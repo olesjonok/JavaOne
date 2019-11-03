d9e54cb7d5bff8aa1fcac5bdeae5f528d2bebf76
olesjonok <54793021+olesjonok@users.noreply.github.com>
2019-11-03 18:38:13 +0200

    Delete UserLoginServiceTest.java

--- src/lv/javaguru/demo/HomeWork/User/UserLoginServiceTest.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.demo.HomeWork.User;
-
-public class UserLoginServiceTest {
-    public static void main(String[] args) {
-        UserLoginService loginPassword = new UserLoginService("0000");
-        loginPassword.getPassword();
-    }
-}
