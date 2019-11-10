673cc6ac1e8017aac7c5cc1808c8a48faf98b922
Olesja Vasiljeva <olesjonok@inbox.lv>
2019-11-10 09:37:11 +0200

    // changes

--- src/lv/javaguru/demo/HomeWork/NumberService/NumberService.java
+++ src/lv/javaguru/demo/HomeWork/NumberService/NumberService.java
@@ -1,16 +1,9 @@
 package lv.javaguru.demo.HomeWork.NumberService;
 
 public class NumberService {
-    private int counter = 0;
-    private int sum = 0;
-
-    public NumberService() {
-        this.counter = counter;
-        this.sum = sum;
-    }
 
     public int rangeSum(int start, int finish) {
-        sum = 0;
+        int sum = 0;
         if (start <= finish) {
             for (int i = start; i <= finish; i++) {
                 sum = sum + i;
@@ -26,7 +19,7 @@ public class NumberService {
     }
 
     public int rangeEvenCount(int start, int finish) {
-        counter = 0;
+        int counter = 0;
         if (start <= finish) {
             for (int i = start; i <= finish; i++) {
                 if (i % 2 == 0) {
@@ -42,23 +35,6 @@ public class NumberService {
         }
         System.out.println(counter);
         return counter;
-    }
-
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public void setCounter(int counter) {
-        this.counter = counter;
-    }
-
-    public int getSum() {
-        return sum;
-    }
-
-    public void setSum(int sum) {
-        this.sum = sum;
     }
 
 }
