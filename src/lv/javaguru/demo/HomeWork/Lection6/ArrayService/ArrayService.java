c975bbc1612ac000c336f3a443f0207f0f628284
Olesja Vasiljeva <olesjonok@inbox.lv>
2019-11-12 22:50:23 +0200

    // changes

---
+++ src/lv/javaguru/demo/HomeWork/Lection6/ArrayService/ArrayService.java
@@ -0,0 +1,43 @@
+package lv.javaguru.demo.HomeWork.Lection6.ArrayService;
+
+import java.util.Random;
+
+public class ArrayService {
+    public int[] create(int size) {
+        int[] allCreate = new int[size];
+        return allCreate;
+    }
+
+    public void fillRandomly(int[] numbers) {
+        Random randomGenerator = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = randomGenerator.nextInt(100);
+        }
+    }
+
+    public void printArray(int[] array) {
+        for (int number : array) {
+            System.out.println("Element = " + number);
+        }
+    }
+
+    public int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public double avg(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return sum / array.length;
+        }
+    }
+}
---
+++ src/lv/javaguru/demo/HomeWork/Lection6/ArrayService/ArrayServiceTest.java
@@ -0,0 +1,71 @@
+package lv.javaguru.demo.HomeWork.Lection6.ArrayService;
+
+import java.sql.SQLOutput;
+
+public class ArrayServiceTest {
+    public static void main(String[] args) {
+        ArrayServiceTest arrayServiceTest = new ArrayServiceTest();
+        arrayServiceTest.testOne();
+        arrayServiceTest.testTwo();
+//        arrayServiceTest.testThree();
+        arrayServiceTest.testFour();
+        arrayServiceTest.testFive();
+
+    }
+
+    public void testOne() {
+        ArrayService arrayService = new ArrayService();
+        int[] newArray = arrayService.create(3);
+        int length = newArray.length;
+        int expectedResult = 3;
+
+        check(length, expectedResult, "Test1");
+        // проверить , что заданная длина == длине массива
+    }
+
+    public void testTwo() {
+        ArrayService arrayService = new ArrayService();
+        int[] numbers = new int[1];
+        arrayService.fillRandomly(numbers);
+        boolean right = true;
+        for (int number : numbers) {
+            if (number >= 0 && number <= 100) {
+                right = true;
+            } else {
+                right = false;
+            }
+        }
+        if (right) {
+            System.out.println("Test2 has passed!");
+        } else {
+            System.out.println("Test2 hasn't passed!");
+        }
+    }
+
+    public void check(int actualResult, int expectedResult, String testName) {
+        if (actualResult == expectedResult) {
+            System.out.println(testName + " has passed!");
+        } else {
+            System.out.println(testName + " has failed!");
+            System.out.println("Expected " + expectedResult + " but was " + actualResult);
+        }
+    }
+
+    public void testThree() {
+    }
+
+    public void testFour() {
+        ArrayService arrayService = new ArrayService();
+        int[] array = {1, 2, 4};
+        int sum = arrayService.sum(array);
+        int expectedResult = 7;
+        check(sum, expectedResult, "Test4");
+    }
+
+    public void testFive() {
+        ArrayService arrayService = new ArrayService();
+        int[] array = {3, 4};
+        double av = arrayService.avg(array);
+        System.out.println(av);
+    }
+}
